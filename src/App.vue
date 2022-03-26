051a2f2cf3bff135f585542bb60ff7c95e70a36c
Ricardo da Rocha <ricardodarocha@outlook.com>
2022-03-26 14:32:50 -0300

    sample

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/agenda.jpg">
+  <img alt="Vue logo" src="./assets/agenda.jpg" width="200">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
